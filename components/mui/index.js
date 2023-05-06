cc8d67d19db4b40e4737b1e539d311a05c94097a
Charlotte Tennant <charlottetennant@Charlotte-Ts-MacBook-Air-2.local>
2023-05-06 14:31:49 +0100

    MUI packages added

---
+++ components/mui/index.js
@@ -0,0 +1,7 @@
+import Button from '@mui/material/Button';
+import HomeIcon from '@mui/icons-material/Home';
+
+export {
+  Button,
+  HomeIcon,
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "@/lib/theme";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
