1b7ebcfc728eb030b9ca4d74518f65d255e0d6ed
Charlotte Tennant <80706604+ctennant718@users.noreply.github.com>
2023-05-09 10:26:56 +0100

    nodemailer functionality added

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,6 +1,37 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const nodemailer = require("nodemailer");
+const { ADMIN_EMAIL } = process.env;
 
-export default function handler(req, res) {
-  console.log(req.body);
-  res.status(200).end()
-}
+const handler = async (req, res) => {
+  let testAccount = await nodemailer.createTestAccount();
+
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: testAccount.user, // generated ethereal user
+      pass: testAccount.pass, // generated ethereal password
+    },
+  });
+
+  const { from, subject, message } = req.body;
+  const msg = {
+    to: from,
+    from: ADMIN_EMAIL,
+    subject,
+    text: message,
+    html: `<p>${message}</p>`,
+  };
+  console.log(msg);
+
+  try {
+    let info = await transporter.sendMail(msg);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info)); //generates a preview URL
+    res.status(201).json({ message: "Email sent.", id: info.messageId });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Failed to send email." });
+  }
+};
+
+export default handler
