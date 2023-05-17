1d185291010f57bde2407d7017fd9f20a7f332ee
Charlotte Tennant <80706604+ctennant718@users.noreply.github.com>
2023-05-17 20:21:14 +0100

    MUI updated

--- components/mui/index.js
+++ components/mui/index.js
@@ -1,11 +1,13 @@
 import Button from "@mui/material/Button";
 import HomeIcon from "@mui/icons-material/Home";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
 import Container from "@mui/material/Container";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
-import CloseIcon from '@mui/icons-material/Close';
+import CloseIcon from "@mui/icons-material/Close";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Drawer from "@mui/material/Drawer";
@@ -15,16 +17,18 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
 import Card from "@mui/material/Card";
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Alert from '@mui/material/Alert';
-import Snackbar from '@mui/material/Snackbar';
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Alert from "@mui/material/Alert";
+import Snackbar from "@mui/material/Snackbar";
 import CircularProgress from "@mui/material/CircularProgress";
 
 export {
   Button,
   HomeIcon,
+  DeleteIcon,
+  EditIcon,
   Container,
   AppBar,
   Box,
