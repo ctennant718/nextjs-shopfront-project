9e1234eb4216589a0f90b15e06b35f732908efc7
Charlotte Tennant <80706604+ctennant718@users.noreply.github.com>
2023-06-12 08:10:23 +0100

    redirect to homepage

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -3,7 +3,7 @@ import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
 export default handleAuth({
   async login(request, response) {
     await handleLogin(request, response, {
-      returnTo: "/profile",
+      returnTo: "/index",
     });
   },
 });
