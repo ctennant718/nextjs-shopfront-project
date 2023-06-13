aeb657dc79ccb8f29576730fec43db5c396f8c9d
Charlotte Tennant <80706604+ctennant718@users.noreply.github.com>
2023-06-13 20:32:05 +0100

    auth update

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,9 +1,3 @@
-import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
+import { handleAuth } from '@auth0/nextjs-auth0';
 
-export default handleAuth({
-  async login(request, response) {
-    await handleLogin(request, response, {
-      returnTo: "/index",
-    });
-  },
-});
+export default handleAuth();
